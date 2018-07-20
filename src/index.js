5b1fbad6e031445b6dbbdb1e064dc81eb0f003e0
Roman <romanchaley@gmail.com>
2018-07-21 00:37:09 +0300

    Got rid of incorrect texture warning

--- src/index.js
+++ src/index.js
@@ -28,11 +28,6 @@ navigator.mediaDevices.getUserMedia(streamOptions).then((stream) => {
 
 webcam.addEventListener('loadeddata', () => {
   gl.bindTexture(gl.TEXTURE_2D, video);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-
   animate();
 });
 
@@ -41,6 +36,10 @@ initTiger(video, gl);
 function animate() {
   requestAnimationFrame(animate);
 
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
   gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, webcam);
   controllersTick();
 
